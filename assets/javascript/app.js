40ee5c575c9773dac6ead497008218e12d5cb687
Bob Brown <robertbernardbrown@gmail.com>
2017-12-18 17:40:16 -0500

    creates a RNG to choose and display a random question from the question array

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,66 +1,74 @@
-var questionObject = {
-    question1: {
-        question: "What movie takes place in NY?",
+var newQuestion
+var questionArray = [
+    question1 = {
+        question: "1What movie takes place in NY?",
         answers: ["Harry Potter", "Sorceror's Stone", "Home Alone", "LOTR"],
         solution: "Home Alone"
     },
-    question2: {
-        question: "What movie takes place in NY?",
+    question2 = {
+        question: "2What movie takes place in NY?",
         answers: ["Harry Potter", "Sorceror's Stone", "Home Alone", "LOTR"],
         solution: "Home Alone"
     },
-    question3: {
-        question: "What movie takes place in NY?",
+    question3 = {
+        question: "3What movie takes place in NY?",
         answers: ["Harry Potter", "Sorceror's Stone", "Home Alone", "LOTR"],
         solution: "Home Alone"
     },
-    question4: {
-        question: "What movie takes place in NY?",
+    question4 = {
+        question: "4What movie takes place in NY?",
         answers: ["Harry Potter", "Sorceror's Stone", "Home Alone", "LOTR"],
         solution: "Home Alone"
     },
-    question5: {
-        question: "What movie takes place in NY?",
+    question5 = {
+        question: "5What movie takes place in NY?",
         answers: ["Harry Potter", "Sorceror's Stone", "Home Alone", "LOTR"],
         solution: "Home Alone"
     },
-    question6: {
-        question: "What movie takes place in NY?",
+    question6 = {
+        question: "6What movie takes place in NY?",
         answers: ["Harry Potter", "Sorceror's Stone", "Home Alone", "LOTR"],
         solution: "Home Alone"
     },
-    question7: {
-        question: "What movie takes place in NY?",
+    question7 = {
+        question: "7What movie takes place in NY?",
         answers: ["Harry Potter", "Sorceror's Stone", "Home Alone", "LOTR"],
         solution: "Home Alone"
     },
-    question8: {
-        question: "What movie takes place in NY?",
+    question8 = {
+        question: "8What movie takes place in NY?",
         answers: ["Harry Potter", "Sorceror's Stone", "Home Alone", "LOTR"],
         solution: "Home Alone"
     },
-    question9: {
-        question: "What movie takes place in NY?",
+    question9 = {
+        question: "9What movie takes place in NY?",
         answers: ["Harry Potter", "Sorceror's Stone", "Home Alone", "LOTR"],
         solution: "Home Alone"
     },
-    question10: {
-        question: "What movie takes place in NY?",
+    question10 = {
+        question: "10What movie takes place in NY?",
         answers: ["Harry Potter", "Sorceror's Stone", "Home Alone", "LOTR"],
         solution: "Home Alone"
     },
-}
+]
 
 var randomQuestion = function () {
-
+    var randomNumber = Math.floor(Math.random() * questionArray.length);
+    console.log(randomNumber)
+    for (var i in questionArray) {
+        if (randomNumber == i) {
+            newQuestion = questionArray[i].question;
+        }
+    }
 }
 
 var startTrivia = function () {
+randomQuestion();
 $('.instructions').addClass('hidden');
 $('.btn').addClass('hidden');
 $('.question-area').append("<div class = 'text-center time-left'> <h2>Seconds to answer: <br> 30</h2></div>");
-$('.question-area').append("<div class = 'text-center question'> <h2>Which movie takes place in NY?</h2></div>");
-// $('.answer').append("")
+$('.question-area').append("<div class = 'text-center question'> <h2>" + newQuestion + "</h2></div>");
+$('.answer').append("")
 }
 
 
