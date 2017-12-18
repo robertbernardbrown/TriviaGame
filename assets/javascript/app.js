44384c88800daeb3cba2797f2cdc3289eea0bffc
Bob Brown <robertbernardbrown@gmail.com>
2017-12-18 16:58:20 -0500

    beginning logic in JS file. Creates placeholder object of questions/answers and removes beginning instructions on button click

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,67 @@
+var questionObject = {
+    question1: {
+        question: "What movie takes place in NY?",
+        answers: ["Harry Potter", "Sorceror's Stone", "Home Alone", "LOTR"],
+        solution: "Home Alone"
+    },
+    question2: {
+        question: "What movie takes place in NY?",
+        answers: ["Harry Potter", "Sorceror's Stone", "Home Alone", "LOTR"],
+        solution: "Home Alone"
+    },
+    question3: {
+        question: "What movie takes place in NY?",
+        answers: ["Harry Potter", "Sorceror's Stone", "Home Alone", "LOTR"],
+        solution: "Home Alone"
+    },
+    question4: {
+        question: "What movie takes place in NY?",
+        answers: ["Harry Potter", "Sorceror's Stone", "Home Alone", "LOTR"],
+        solution: "Home Alone"
+    },
+    question5: {
+        question: "What movie takes place in NY?",
+        answers: ["Harry Potter", "Sorceror's Stone", "Home Alone", "LOTR"],
+        solution: "Home Alone"
+    },
+    question6: {
+        question: "What movie takes place in NY?",
+        answers: ["Harry Potter", "Sorceror's Stone", "Home Alone", "LOTR"],
+        solution: "Home Alone"
+    },
+    question7: {
+        question: "What movie takes place in NY?",
+        answers: ["Harry Potter", "Sorceror's Stone", "Home Alone", "LOTR"],
+        solution: "Home Alone"
+    },
+    question8: {
+        question: "What movie takes place in NY?",
+        answers: ["Harry Potter", "Sorceror's Stone", "Home Alone", "LOTR"],
+        solution: "Home Alone"
+    },
+    question9: {
+        question: "What movie takes place in NY?",
+        answers: ["Harry Potter", "Sorceror's Stone", "Home Alone", "LOTR"],
+        solution: "Home Alone"
+    },
+    question10: {
+        question: "What movie takes place in NY?",
+        answers: ["Harry Potter", "Sorceror's Stone", "Home Alone", "LOTR"],
+        solution: "Home Alone"
+    },
+}
 
+var randomQuestion = function () {
 
+}
 
 var startTrivia = function () {
 $('.instructions').addClass('hidden');
-$('.btn-start').addClass('hidden');
+$('.btn').addClass('hidden');
+$('.question-area').append("<div class = 'text-center time-left'> <h2>Seconds to answer: <br> 30</h2></div>");
+$('.question-area').append("<div class = 'text-center question'> <h2>Which movie takes place in NY?</h2></div>");
+// $('.answer').append("")
 }
 
 
-$('.start-btn').on('click', startTrivia());
+$('.start-btn').click(startTrivia);
